39cde6221e83172c2f16ad034bef8bdbbc6d4a21
Caffeinew <tarabrin181@mail.ru>
2021-05-23 03:32:06 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const path = require('path')
+
+module.exports = {
+  mode: "production",
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|webp|svg)/i,
+        type: "asset",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(s[ac]|c)ss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+    template: "./src/index.html"
+  })],
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+};
